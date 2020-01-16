f529306bf8b7e1587ebd45cbd701ec5b1eb857ea
Hernandez Zepeda <alejandro.zepeda@bocar.com>
2020-01-16 16:04:02 -0600

    Modified operators

--- models/operator.js
+++ models/operator.js
@@ -8,7 +8,10 @@ var Schema = mongoose.Schema;
 //El esquema de Operator tendra los siguientes atributos
 var OperatorSchema = Schema({
 
-    employee: { type: Schema.ObjectId, ref: 'Employee' }
+    employee: { type: Schema.ObjectId, ref: 'Employee' },
+    square: { type : Schema.ObjectId, ref: 'Square' },
+    machine: { type: Schema.ObjectId, ref: 'Machine' },
+    turn: { type: Schema.ObjectId, ref: 'Turn' }
 });
 
 //Exportaremos el modelo para usarlo en otros archivos
